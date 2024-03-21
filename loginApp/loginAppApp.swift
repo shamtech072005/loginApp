2d2270a68a5cfaeff5a80676a8dc315f4e5e3353
shamtech072005 <shamedward2@gmail.com>
2024-03-21 15:16:09 +0530

    Initial Commit

---
+++ loginApp/loginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginAppApp.swift
+//  loginApp
+//
+//  Created by shamtech07 on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct loginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
